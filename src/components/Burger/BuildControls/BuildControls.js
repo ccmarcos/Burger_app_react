3903920941e66393ec1444ec4ed400a32d7ceca0
ccmarcos <marcosantonio199189@gmail.com>
2019-04-05 11:04:01 -0600

    price summary and modal

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -10,6 +10,7 @@ const controls = [
 
 const buildControls = (props) => (
 	<div className="BuildControls">
+	<p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
 		{controls.map(ctrl => (
 			<BuildControl 
 				key={ctrl.label} 
@@ -19,6 +20,10 @@ const buildControls = (props) => (
 				removed={() => props.ingredientRemove(ctrl.type)}
 				disabled={props.disabled[ctrl.type]} />
 		))}
+		<button 
+			className="OrderButton"
+			disabled={!props.purchasable}
+			onClick={props.ordered}>ORDER NOW</button>
 	</div>
 );
 
