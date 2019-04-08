5b8d08d019e65f0055a35b981506d4cb7ffce490
ccmarcos <marcosantonio199189@gmail.com>
2019-04-08 10:26:12 -0500

    SideDrawer responsive design

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux';
 
 const sideDrawer = (props) => {
-	//..
+	let attachedClasses = ["SideDrawer", "Close"];
+	if(props.open) {
+		attachedClasses = ["SideDrawer", "Open"];
+	}
+
+
 	return(
-		<div className="sideDrawer">
-			<Logo />			
-			<nav>
-				<NavigationItems />
-			</nav>
-		</div>
+		<Aux>
+			<Backdrop show={props.open} clicked={props.closed}/>
+			<div className={attachedClasses.join(' ')}>
+				<div className="Logo">
+					<Logo />			
+				</div>
+				<nav>
+					<NavigationItems />
+				</nav>
+			</div>
+		</Aux>
 	);
 }
 
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -5,8 +5,10 @@ import NavigationItems from '../NavigationItems/NavigationItems';
 const toolbar = (props) => (
 	<header className="Toolbar">
 		<div>MENU</div>
-		<Logo />
-		<nav>
+		<div className="Logo">
+			<Logo />
+		</div>
+		<nav className="DesktopOnly">
 			<NavigationItems />
 		</nav>
 	</header>
